2bf820b58b3ce2262ec9a88b3d36536a3b302cbb
Yash Soni <yashsoni2017@gmail.com>
2017-10-14 12:56:23 +0530

    Update FindingProperNamesFromABook.py

--- FindingNamesFromPassage/FindingProperNamesFromABook.py
+++ FindingNamesFromPassage/FindingProperNamesFromABook.py
@@ -1,7 +1,7 @@
 from FSC import Userdata
 from collections import Counter
 
-file_1 = open("C:\\Users\\chinkasoni\\Desktop\\testytest.txt","r")
+file_1 = open("C:\\Users\\chinkasoni\\Desktop\\testytest.txt","r")  # Path of txt file of the book
 foo = Userdata()
 file_read = file_1.read()
 
@@ -9,8 +9,15 @@ file_read= foo.FilterData(file_read)
 file_write = open("testfile3.txt","w")
 file_write.write(file_read)
 data=[]
-file_2 = open("C:\\Users\\chinkasoni\\AppData\\Local\\Programs\\Python\\Python36\\testfile2.txt","r")
+file_2 = open("C:\\Users\\chinkasoni\\AppData\\Local\\Programs\\Python\\Python36\\testfile2.txt","r") # Python Directory
 file_3 = file_2.read()
+if(os.stat("C:\\Users\\chinkasoni\\AppData\\Local\\Programs\\Python\\Python36\\testfile4.txt").st_size == 0):
+        file_write = open("testfile4.txt",'w')
+        file_write.write(file_read)
+        data=[]
+        file_2 = open("C:\\Users\\chinkasoni\\AppData\\Local\\Programs\\Python\\Python36\\testfile4.txt","r")
+        file_3 = file_2.read()
+
 datos = file_3.split()
 for i in range(0,len(datos)):
 	if(datos[i].istitle() and len(datos[i])>4 and len(datos[i])<20):
