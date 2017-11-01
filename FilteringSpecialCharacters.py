762c8e217b7666f5e61d6ff2a72d7720f8c1a318
Yash Soni <yashsoni2017@gmail.com>
2017-11-02 00:27:49 +0530

    Update FilteringSpecialCharacters.py

--- FilteringSpecialCharacters.py
+++ FilteringSpecialCharacters.py
@@ -1,23 +1,64 @@
 import numpy
 
-comma = [",", "?", "!", ":", ";", "/", ".", "@", "#", "$", "%", "^", "&", "*"] # All the characters that will be filter out
-data = []
-text = input("Ask me something: \n")
+class Userdata:
+    def __init__(self, dd=""):
+        self.dd = dd
 
-f = list(text)
+    def FilterData(self, ddd=""):
+        comma = [",", "?", "!", ":", ";", "/", ".", "@", "#", "$", "%", "^", "&", "*","\"","\\","_","-","(",")","\'s","[","]"] # All the characters that will be filter out
+        data = []
+        text = ddd
+        f = list(text)
+        for i in range(0,len(comma)):
+            for j in range(0,len(text)):
+                if(comma[i] == f[j]):
+                    data.append(int(j))
+        data = sorted(data)
+        data2 = numpy.array(data)
+        for k in range(0,len(data2)):
+            del f[int(data2[k])]
+            data2=data2-1
 
-for i in range(0, len(comma)):
-    for j in range(0, len(text)):
-        if(comma[i] == f[j]):
-            data.append(int(j))
-         
-data = sorted(data)
-data2 = numpy.array(data)
+        d = ''.join(f)
+        return d
 
-for k in range(0, len(data2)):
-    del f[int(data2[k])]
-    data2 = data2 - 1
+    def SensorWords(self,dato=""):
+        words = ['fuck', 'shit', 'bhenchod', 'madarchod','lode','chutiye','asshole','motherfucker', 'motherfucking', 'fucking', 'Fuck','fucks', 'shits', 'bhenchods', 'madarchods','lodes','chutiyes','assholes','motherfuckers', 'motherfucking', 'fucking', 'Fucks']
+        data=[]
+        listo = dato.split()
+        for i in range(0,len(listo)):
+            for j in range(0,len(words)):
+                if(words[j] == listo[i]):
+                    l = len(listo[i])-1
+                    g = list(listo[i])
+                    for j in range(1,l):
+                            del g[j]
+                            g.insert(j,'*')
+                    j = ''.join(g)
+                    del listo[i]
+                    listo.insert(i,j)
+        d = ' '.join(listo)
+                    
 
+        return d
 
-d = ''.join(f)
-print(d)
+    def FilterOut(self, datc=""):
+        comma = ['fuck', 'shit', 'bhenchod', 'madarchod','lode','chutiye','asshole','motherfucker', 'motherfucking', 'fucking', 'Fuck','fucks', 'shits', 'bhenchods', 'madarchods','lodes','chutiyes','assholes','motherfuckers', 'motherfucking', 'fucking', 'Fucks']# All the words that will be filter out
+        data = []
+        text = datc
+        f = text.split()
+        for i in range(0,len(f)):
+            for j in range(0,len(comma)):
+                if(comma[j] == f[i]):
+                    data.append(i)
+        data = sorted(data)
+        data2 = numpy.array(data)
+        for k in range(0,len(data2)):
+            del f[int(data2[k])]
+            data2=data2-1
+
+        d = ' '.join(f)
+        return d
+        
+        
+
