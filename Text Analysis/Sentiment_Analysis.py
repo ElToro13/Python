83219b96107a40515872eb9e7bdd1e33ec8e1909
Yash Soni <yashsoni2017@gmail.com>
2017-11-07 19:40:49 +0530

    Update Sentiment_Analysis.py

--- Text Analysis/Sentiment_Analysis.py
+++ Text Analysis/Sentiment_Analysis.py
@@ -1,60 +1,126 @@
+import re
 import tweepy
-from geopy.geocoders import Nominatim
-geolocator = Nominatim()
-location = geolocator.geocode("Vadodara, Gujarat, India")
+from tweepy import OAuthHandler
 from textblob import TextBlob
-
-# Consumer keys and access tokens, used for OAuth
-consumer_key        = ' '
-consumer_secret     = ' '
-access_token        = ' '
-access_token_secret = ' '
-
-# OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-# Creation of the actual interface, using authentication
-api = tweepy.API(auth)
-
-# Send the tweet
-#message = 'This is a test'
-#api.update_status(message)
-#print(api.trends_closest(location.latitude, location.longitude))
-#dd = api.trends_place(2295402) # WOEID of your location (WOEID = Where on earth ID)
-#for i in range(0,10):          # Returns top 10 Trends for the place
-#    print(dd[0]['trends'][i]['name'])
-
-def sentiment_analysis(quote, num_tweets):
-    # Checks if the sentiment for our quote is
-    # positive or negative, returns True if
-    # majority of valid tweets have positive sentiment
-    list_of_tweets = api.search(quote, count=num_tweets)
-    positive, null = 0, 0
-    #dd = Userdata()
-    for tweet in list_of_tweets:
-        #ww = dd.FilterData(tweet.text)
+ 
+class TwitterClient(object):
+    '''
+    Generic Twitter Class for sentiment analysis.
+    '''
+    def __init__(self):
+        '''
+        Class constructor or initialization method.
+        '''
+        # keys and tokens from the Twitter Dev Console
+        consumer_key = 'E1JNN3IMIh6F6bmsK2BQOGBhP'
+        consumer_secret = 'm6coRZ7eEc4GzybqezZmjPnWRb6FA1MhCUKo0QVuulWyqdWOrk'
+        access_token = '1374602324-IRs1aPkpDUIumTfNcSmoHcmQWjge5IYpoTVnc2y'
+        access_token_secret = '263wMQvZ6JJsxtpMAOZAGivIBPgFbvcejYuuKNBJqLgme'
+ 
+        # attempt authentication
+        try:
+            # create OAuthHandler object
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            # set access token and secret
+            self.auth.set_access_token(access_token, access_token_secret)
+            # create tweepy API object to fetch tweets
+            self.api = tweepy.API(self.auth)
+        except:
+            print("Error: Authentication Failed")
+ 
+    def clean_tweet(self, tweet):
+        '''
+        Utility function to clean tweet text by removing links, special characters
+        using simple regex statements.
+        '''
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+ 
+    def get_tweet_sentiment(self, tweet):
+        '''
+        Utility function to classify sentiment of passed tweet
+        using textblob's sentiment method
+        '''
+        # create TextBlob object of passed tweet text
+        analysis = TextBlob(self.clean_tweet(tweet))
+        # set sentiment
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+ 
+    def get_tweets(self, query, count = 10):
+        '''
+        Main function to fetch tweets and parse them.
+        '''
+        # empty list to store parsed tweets
+        tweets = []
+ 
+        try:
+            # call twitter api to fetch tweets
+            fetched_tweets = self.api.search(q = query, count = count)
+ 
+            # parsing tweets one by one
+            for tweet in fetched_tweets:
+                # empty dictionary to store required params of a tweet
+                parsed_tweet = {}
+ 
+                # saving text of tweet
+                parsed_tweet['text'] = tweet.text
+                # saving sentiment of tweet
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+ 
+                # appending parsed tweet to tweets list
+                if tweet.retweet_count > 0:
+                    # if tweet has retweets, ensure that it is appended only once
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+ 
+            # return parsed tweets
+            return tweets
+ 
+        except tweepy.TweepError as e:
+            # print error (if any)
+            print("Error : " + str(e))
+ 
+def main():
+    # creating object of TwitterClient Class
+    api = TwitterClient()
+    # calling function to get tweets
+    ss = input('Enter Search Query: ')
+    tweets = api.get_tweets(query = ss, count = 2000)
+ 
+    # picking positive tweets from tweets
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    # percentage of positive tweets
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+    # picking negative tweets from tweets
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    # percentage of negative tweets
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+    # percentage of neutral tweets
+    print("Neutral tweets percentage: {} % ".format(100*(len(tweets) - len(ntweets) - len(ptweets))/len(tweets)))
+ 
+    # printing first 5 positive tweets
+    print("\n\nPositive tweets:")
+    for tweet in ptweets[:10]:
+        print(tweet['text'].encode('utf-8'))
+ 
+    # printing first 5 negative tweets
+    print("\n\nNegative tweets:")
+    for tweet in ntweets[:10]:
+        print(tweet['text'].encode('utf-8'))
+ 
+if __name__ == "__main__":
+    # calling main function
+    while True:
         
-        blob = TextBlob(tweet.text).sentiment
-        if not blob.subjectivity == 0:
-            print(tweet.text.encode('utf-8'))
-        
-            if blob.subjectivity == 0:
-                null += 1
-                next
-            if blob.polarity > 0:
-                print(blob.polarity)
-                print()
-                positive += 1
-            else:
-                print(blob.polarity)
-                print()
-            
-    if positive > ((num_tweets - null)/2):
-        return True
-    else:
-        return False
-    
+        try:
+            main()
+        except:
+            print("No Tweets")
 
-print(sentiment_analysis('BJP',50))
 
