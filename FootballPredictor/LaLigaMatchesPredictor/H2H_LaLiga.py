3ed7c46e173b9dcbc5d0e31f09653a36f98b8aae
Yash Soni <yashsoni2017@gmail.com>
2017-12-03 23:54:58 +0530

    Update H2H_LaLiga.py

--- FootballPredictor/LaLigaMatchesPredictor/H2H_LaLiga.py
+++ FootballPredictor/LaLigaMatchesPredictor/H2H_LaLiga.py
@@ -21,17 +21,17 @@ def H2H_Cal(HT='',AT=''):
     #print(str(HTT)+" / "+str(ATT))
                
     #print('---------------')
-    for i in range(0,len(df['homeTeam'])):
-        if(df.homeTeam[i]==AT):
-            if(df.awayTeam[i] ==HT):
-                total+=1
-                #print(str(df.homeGoalFT[i]) + " - " + str(df.awayGoalFT[i]) + " // "+str(df.date[i]) )
-                if((int(df.homeGoalFT[i])-int(df.awayGoalFT[i]))>0):
-                    ATT+=3
-                elif((df.homeGoalFT[i]-df.awayGoalFT[i])<0):
-                    HTT+=3
-                else:
-                    HTT+=1
-                    ATT+=1
+#    for i in range(0,len(df['homeTeam'])):
+#        if(df.homeTeam[i]==AT):
+#            if(df.awayTeam[i] ==HT):
+#                total+=1
+#                #print(str(df.homeGoalFT[i]) + " - " + str(df.awayGoalFT[i]) + " // "+str(df.date[i]) )
+#                if((int(df.homeGoalFT[i])-int(df.awayGoalFT[i]))>0):
+#                    ATT+=3
+#                elif((df.homeGoalFT[i]-df.awayGoalFT[i])<0):
+#                    HTT+=3
+#                else:
+#                    HTT+=1
+#                    ATT+=1
     return HTT,ATT,total*3
 
