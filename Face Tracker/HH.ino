b5e30d91f4456e6a58a93479f9e6f5136038dce9
Yash Soni <yashsoni2017@gmail.com>
2019-04-10 00:26:02 +0100

    Add files via upload

---
+++ Face Tracker/HH.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+Servo myservo;
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(13,OUTPUT);
+myservo.attach(9);
+}
+
+int c;
+void loop() {
+  
+  if(Serial.available()>0){
+    c = Serial.read();
+    myservo.write(c);
+  }
+  
+}  
